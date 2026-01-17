9712372d3c262b742732718b57271fb922f71d9f
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-17 20:28:53 +0000

    Added my app pages to settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pages',                            #added app (common fuctionality) called pages here
 ]
 
 MIDDLEWARE = [
