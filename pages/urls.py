341c97633861d80b0667501061cf4699df32f51c
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-21 17:16:21 +0000

    added the path url parameters into the urls.py file so that it reaches a user_profile view and transforms all numbers after users/id to integers with int:id

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('users/<int:id>/', views.user_profile)
 ]
