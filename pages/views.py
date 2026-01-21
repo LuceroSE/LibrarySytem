d6363e06903cf5b9bf0edb202046f1a6e35316b2
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-21 16:29:16 +0000

    created a context python list that stores the number list

--- pages/views.py
+++ pages/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "home.html", {'username':'John'})
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    return render(request, "home.html", {'numbers': numbers})
 
 def about(request):
     return render(request, "about.html")
