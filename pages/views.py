fb06b0e51fcd46298cf52d6f6b1e5156d373a710
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-21 17:50:54 +0000

    Added the user_profile function for the profile.html to test the url patterns

--- pages/views.py
+++ pages/views.py
@@ -10,3 +10,16 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+def user_profile(request, id):
+    users = {
+        1: {'name': 'Alice', 'email':'alice@example.com'},
+        2: {'name': 'Bob', 'email': 'bob@example.com'},
+        3: {'name': 'Charlie', 'email': 'charlie@example.com'}
+    }
+
+    user = users.get(id) #.get returns none if key does not exit and you didnt specidy a default return statement
+
+    if user is None: 
+        return render(request, 'not_found.html', {'id': id}) 
+    
+    return render(request, 'profile.html', {'user_id':id})
