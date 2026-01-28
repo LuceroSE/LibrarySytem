1e8de529189bc9518fe0731d6781e1b539f3ca4a
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-28 19:16:53 +0000

    created the models Authos and Book for create our tables in the database

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Author(model.Model):
+    name = models.CharField(max_length=100)
+    birth_year = models.IntergerField()
+    country = models.CharField(max_length=50)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    year_published = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) # This creates the author_id foreign key column
