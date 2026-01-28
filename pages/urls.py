b62cad74f68c24241d1520ddb6f74e5971298bbc
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-28 23:07:25 +0000

    added a path to the author template using url parameters

--- pages/urls.py
+++ pages/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('users/<int:id>/', views.user_profile),
     path('books/', views.book_list, name='book_list'),
-    path('books/search/', views.book_search, name='book_search'), #before the books/<int:book_id>/' path because Otherwise Django might try to interpret "search" as a book ID.
-    path('books/<int:book_id>/', views.book_detail, name='book_detail')
+    path('books/search/', views.book_search, name='book_search'), #before the books/<int:book_id>/' path because Otherwise Django might try to interpret "search" as a book ID. THIS IS GET, NOT A URL PARAMETER
+    path('books/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('authors/<int:author_id>/', views.author_detail, name='author_detail')
 ]
