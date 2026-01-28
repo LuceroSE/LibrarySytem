548faf76eec343666b57fdc0a38cc062f31318ac
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-28 20:23:19 +0000

    added the url path to the book_list template

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('users/<int:id>/', views.user_profile)
+    path('users/<int:id>/', views.user_profile),
+    path('books/', views.book_list, name='book_list')
 ]
