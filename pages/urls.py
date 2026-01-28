7e64335a926daea74fd607166b9ca9221763aa0e
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-01-28 22:02:40 +0000

    added the path to the search template, before the path to book_detail view to avoid matching search to int

--- pages/urls.py
+++ pages/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('users/<int:id>/', views.user_profile),
     path('books/', views.book_list, name='book_list'),
+    path('books/search/', views.book_search, name='book_search'), #before the books/<int:book_id>/' path because Otherwise Django might try to interpret "search" as a book ID.
     path('books/<int:book_id>/', views.book_detail, name='book_detail')
 ]
