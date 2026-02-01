50974c577105b67c452e07d6a37f966674e7e942
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-02-01 17:46:15 +0000

    made the template to add a book

--- pages/urls.py
+++ pages/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('users/<int:id>/', views.user_profile),
     path('books/', views.book_list, name='book_list'),
     path('books/search/', views.book_search, name='book_search'), #before the books/<int:book_id>/' path because Otherwise Django might try to interpret "search" as a book ID. THIS IS GET, NOT A URL PARAMETER
+    path('books/add/', views.add_book, name='add_book'), #Django checks URL patterns in order from top to bottom. If <int:book_id> came first, Django would try to interpret "add" as a book ID, fail (it's not an integer), and return a 404 error.
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
     path('authors/<int:author_id>/', views.author_detail, name='author_detail')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book, Author
+from .forms import BookForm
 
 # Create your views here.
 
@@ -45,3 +46,18 @@ def author_detail(request, author_id):
     author = get_object_or_404(Author, id=author_id)
     books = Book.objects.filter(author=author)
     return render(request, 'author_detail.html', {'author':author, 'books':books})
+
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST) #request.POST is a dictionary that store the fields as keys and the values inputted as values
+        if form.is_valid():
+            form.save()
+            return redirect('book_list') #return to the url of book_list using the nickname of the path mapping
+            #we need this return or otherwise we will stay in the same page and everything will be reloaded when we refresh and
+            #the book will be resaved, creating duplicates
+    else:#if it is get, lets reply with the book form (inside this statement the form is empty)
+        form = BookForm()
+    return render(request, 'add_book.html', {'form' : form})
+    # we get to this return statement because the form is new (we just created it we are sending it back for our get response)
+    # or because form was invalid and the responde is an invalid form (with fields filled in), user fills the fields and fixes the issues
+    # after that another post request is created by them pressing the submit boton
