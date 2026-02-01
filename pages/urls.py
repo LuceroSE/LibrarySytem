9fd936de496e92d3277da0d9607f463358699648
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-02-01 19:53:13 +0000

    added path to view that takes registration

--- pages/urls.py
+++ pages/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('books/search/', views.book_search, name='book_search'), #before the books/<int:book_id>/' path because Otherwise Django might try to interpret "search" as a book ID. THIS IS GET, NOT A URL PARAMETER
     path('books/add/', views.add_book, name='add_book'), #Django checks URL patterns in order from top to bottom. If <int:book_id> came first, Django would try to interpret "add" as a book ID, fail (it's not an integer), and return a 404 error.
     path('books/<int:book_id>/', views.book_detail, name='book_detail'),
-    path('authors/<int:author_id>/', views.author_detail, name='author_detail')
+    path('authors/<int:author_id>/', views.author_detail, name='author_detail'),
+    path('regiter/', views.register, name='register')
 ]
