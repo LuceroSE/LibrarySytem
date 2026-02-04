3ce1243e97d880d4aaf66d24f37b970dca35c0f0
Lucero Sangueza <lucero.sanguezaespinosa2@mail.dcu.ie>
2026-02-04 19:07:13 +0000

    i

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book, Author
 from .forms import BookForm
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.http import HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -50,14 +50,15 @@ def author_detail(request, author_id):
     books = Book.objects.filter(author=author)
     return render(request, 'author_detail.html', {'author':author, 'books':books})
 
-# for the user_passes_test decorator
+# for the user_passes_test decorator, lambda is this too
 def is_staff(user):
     return user.is_staff
 
-@user_passes_test(is_staff) #decorator to check if user passes this test of is staff, we can define here any condition we want tho
 @login_required # only return the add book (in a wrapper) html if user is logged in. login_required is a 
 #decorator and it returns our function and lets access to the add_book page only if the user is loggged in otherwise it defaults to 
 # LOGIN_URL = 'login' #setting for where to send users who try to access a page that requires login 
+@permission_required('pages.add_book', raise_exception=True)  #checks whether the user has a specific permission, if a logged-in user lacks permission, a 403 makes more sense than sending them to a login page they don't need
+@user_passes_test(is_staff) #decorator to check if user passes this test of is staff, we can define here any condition we want noy only is_staff tho. redirection is to login page rn, a 403 makes more sense than sending them to a login page they don't need 
 def add_book(request):
     
     if request.method == 'POST':
